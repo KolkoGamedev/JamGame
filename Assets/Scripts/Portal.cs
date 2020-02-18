db3bca70ef6fddb34ae09463821e03331a21cbe3
Syberian <mixux98@gmail.com>
2020-02-18 20:03:24 +0100

    Sprites remaster

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private string levelToLoad = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
