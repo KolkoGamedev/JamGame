41f53592405c4e4d40fd6633023e4c304f0f1ec6
Syberian <mixux98@gmail.com>
2020-02-18 15:24:04 +0100

    Added Shooting

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float shootPower = 200;
+
+    private Vector3 _shootStartingPoint;
+    private Vector3 _shootEndingPoint;
+    private Rigidbody2D rb;
+    private Camera _mainCam;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        _mainCam = Camera.main;
+    }
+
+    private void Start()
+    {
+        DragIndicator.OnDragFinish += Shoot;
+    }
+
+    private void Shoot(Vector3 direction)
+    {
+        Debug.Log(direction * shootPower);
+        rb.AddForce(direction * shootPower);
+    }
+}
