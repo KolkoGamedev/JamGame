275974877ffc5ddfbe5e99855a5d29005f4c71a3
Blazej <a.blazej8@gmail.com>
2020-02-20 16:29:02 +0100

    Button prefab

--- Assets/Scripts/ButtonEvent.cs
+++ Assets/Scripts/ButtonEvent.cs
@@ -11,7 +11,7 @@ public class ButtonEvent : MonoBehaviour
     {
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
