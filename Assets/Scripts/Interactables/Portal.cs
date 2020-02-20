415190f4681a09895c2c8d2ff87ab716c51c8d1a
Syberian <mixux98@gmail.com>
2020-02-20 16:28:35 +0100

    Added end game light

--- Assets/Scripts/Interactables/Portal.cs
+++ Assets/Scripts/Interactables/Portal.cs
@@ -4,7 +4,7 @@ using System;
 
 public class Portal : MonoBehaviour
 {
-    public static event Action OnLevelComplete = delegate { };
+    public static event Action<GameObject> OnLevelComplete = delegate { };
 
     [SerializeField] private string levelToLoad = null;
 
@@ -12,9 +12,9 @@ public class Portal : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            OnLevelComplete();
+            OnLevelComplete(collision.gameObject);
             collision.gameObject.GetComponent<Dissolve>().StartPlayerDissolve();
-           
+
             //SceneManager.LoadScene(levelToLoad);
         }
     }
