4a680fa1819b13b4e5a6ca848dcc5fab00a5d43c
Syberian <mixux98@gmail.com>
2020-02-20 20:18:28 +0100

    Fix clickable Button i hideableWall

---
+++ Assets/Scripts/ClickableButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClickableButton : MonoBehaviour
+{
+    [SerializeField] private GameObject ToInteract;
+    public static event Action OnButtonClick = delegate { };
+
+    private bool isPushed = false;
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isPushed)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                isPushed = true;
+                OnButtonClick();
+
+                transform.localPosition = new Vector3(0, -0.1f, 0);
+
+                if (ToInteract != null)
+                ToInteract.GetComponent<IInteractable>().Act();
+            }
+        }
+
+    }
+}
+
+public interface IInteractable
+{
+    void Act();
+}
---
+++ Assets/Scripts/HideableWall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideableWall : MonoBehaviour, IInteractable
+{
+    public void Act()
+    {
+        //Hide Wall
+    }
+}
