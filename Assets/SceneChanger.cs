6d0756672d3b68351ee90cf7dfdb3f666d9beb4f
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2020-02-21 21:38:11 +0100

    Added win and loose screen

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
