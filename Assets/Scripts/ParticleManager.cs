14da926c9af8cd655768ec58a05108e222f12e75
Syberian <mixux98@gmail.com>
2020-02-22 17:48:28 +0100

    Added death particles

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -7,11 +7,13 @@ public class ParticleManager : MonoBehaviour
     [SerializeField] private GameObject OnHitParticles = null;
     [SerializeField] private GameObject OnWallHitStep = null;
     [SerializeField] private GameObject OnWallHitFinal = null;
+    [SerializeField] private GameObject OnDeath = null;
 
     private void Awake()
     {
         PlayerHealth.OnHit += SpawnOnHitParticles;
         DestroyableWall.OnWallAttack += SpawnOnWallHitStepParticles;
+        PlayerHealth.OnDie += SpawnOnDeathParticles;
     }
 
     private void SpawnOnHitParticles(int v)
@@ -29,6 +31,11 @@ public class ParticleManager : MonoBehaviour
         {
             GameObject particle = Instantiate(OnWallHitFinal, x.gameObject.transform.position, Quaternion.identity, null);
         }
+    }
+
+    private void SpawnOnDeathParticles()
+    {
+        GameObject particle = Instantiate(OnDeath, PlayerMovement.Instance.gameObject.transform.position, Quaternion.identity, null);
     }
 
 }
