64a1ea809eef56bba19f6e38c45173b150c2be6c
Syberian <mixux98@gmail.com>
2020-02-22 18:10:40 +0100

    Added level1

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -17,6 +17,7 @@ public class HealthUI : MonoBehaviour
         heartIndex = HeartImgs.Length-1;
 
         PlayerHealth.OnHit += RemoveHeart;
+        PlayerHealth.OnHeal += AddHeart;
         PlayerUtilities.OnShield += ShieldHeart;
         PlayerUtilities.OnUnShield += UnshieldHeart;
     }
@@ -29,6 +30,17 @@ public class HealthUI : MonoBehaviour
             HeartImgs[heartIndex--].gameObject.SetActive(false);
         }
     }
+    private void AddHeart(int value)
+    {
+        if(heartIndex <= 3)
+        {
+            for(int i = 0; i < value; i++)
+            {
+                HeartImgs[heartIndex + 1].gameObject.SetActive(true);
+            }
+        }
+    }
+
     private void ShieldHeart()
     {
         HeartImgs[heartIndex].sprite = shieldedHeart;
