b3c1a0cabf739cbdf32f1fa3c689b32dad331218
Syberian <mixux98@gmail.com>
2020-02-27 15:55:32 +0100

    Preparign for MapGenerator

---
+++ Assets/Scripts/Maps/MapGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [Header("Map Parts")]
+    [SerializeField] private List<GameObject> startingRooms = null;
+    [SerializeField] private List<GameObject> rooms = null;
+    [SerializeField] private List<GameObject> connectors = null;
+    [SerializeField] private List<GameObject> bossRooms = null;
+    [SerializeField] private List<GameObject> endRooms = null;
+    [Space]
+    [SerializeField] private int roomCount = 2;
+
+    private void Awake()
+    {
+        if (startingRooms == null)
+            Debug.LogWarning("Fill starting rooms before proceeding!");
+        else
+            SpawnMapPart(GetRandomRoomPart(startingRooms));
+            
+    }
+
+    private GameObject GetRandomRoomPart(List<GameObject> pool)
+    {
+        return pool[Random.Range(0, pool.Count)];
+    }
+    private void SpawnMapPart(GameObject part)
+    {
+        GameObject _spawned = Instantiate(part);
+    }
+}
