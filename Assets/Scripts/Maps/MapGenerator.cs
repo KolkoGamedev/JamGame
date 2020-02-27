589636df91be3f3a28555972b38245aad1b7f353
Syberian <mixux98@gmail.com>
2020-02-27 18:30:40 +0100

    Reorganisation and map gen

--- Assets/Scripts/Maps/MapGenerator.cs
+++ Assets/Scripts/Maps/MapGenerator.cs
@@ -15,27 +15,35 @@ public class MapGenerator : MonoBehaviour
 
     private List<Transform> _lastSpawnedEndingPoints;
 
+    private void Awake()
+    {
+        //_lastSpawnedEndingPoints.Add();
+        SpawnMapPart(startingRooms);
+    }
+
     private GameObject GetRandomRoomPart(List<GameObject> pool)
     {
         return pool[Random.Range(0, pool.Count)];
     }
-
-    //todo
-    private void SpawnMapPart(GameObject part)
+    private Transform GetRandomEndPointTransform()
     {
-        GameObject _spawned = Instantiate(part);
+        return _lastSpawnedEndingPoints[Random.Range(0, _lastSpawnedEndingPoints.Count)];
+    }
+    //todo
+    private void SpawnMapPart(List<GameObject> part)
+    {
+        GameObject _spawned = Instantiate(GetRandomRoomPart(part), GetRandomEndPointTransform().position, Quaternion.identity);
     }
 
     private Transform GetPartStartingPoint(GameObject part) => part.GetComponentInChildren<LevelStart>().transform;
     //Make list of all endings
-    private Transform GetPartEndingPoint(GameObject part)
+    private void GetPartEndingPoints(GameObject part)
     {
-        //Iterate through children and get their endLevel transforms, add to list
-        for(int i = 0; i < part.transform.childCount; i++)
+        //Can try to change it to for, but will be less clean
+        foreach(GameObject x in part.transform)
         {
-            //_lastSpawnedEndingPoints.Add();
+            _lastSpawnedEndingPoints.Add(x.GetComponent<LevelEnd>().transform);
         }
-       return part.GetComponentInChildren<LevelEnd>().transform;
     }
 
 }
