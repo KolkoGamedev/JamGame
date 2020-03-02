5e824b371acc2a3a06323cc204982102cb4f0665
Syberian <mixux98@gmail.com>
2020-03-03 00:21:26 +0100

    Added DOTween

--- Assets/Scripts/Player/LightManager.cs
+++ Assets/Scripts/Player/LightManager.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class LightManager : MonoBehaviour
 {
     [SerializeField] private GameObject mask = null;
+    [SerializeField] private int startingScale = 5;
     private Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    private void Start()
+    {
+        PlayerMovement.OnShoot += StartChangingSize;
+    }
+    private void StartChangingSize()
+    {
+        float x = startingScale + rb.velocity.magnitude;
+        mask.transform.DOScale(new Vector3(x, x, x), .5f);
+    }
 
-
+    private void GoBack()
+    {
+        mask.transform.DOScale(new Vector3(5f, 5f, 5f), 3f);
+    }
 }
